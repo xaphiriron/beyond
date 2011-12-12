xaphiriron/beyond

commit cd81b21362e140123405b448bbac699b48910bb2

Camera will no longer update on "positionUpdate" messages when the message is not from the entity it's focused on.

src/component_camera.c
@@ -470,8 +470,13 @@ void component_cameraPositionResponse (EntComponent camera, EntSpeech speech)
 {
 	Entity
 		camEntity = component_entityAttached (camera);
+	cameraComponent
+		camData = component_getData (camera);
 	POSITIONUPDATE
 		update = speech->arg;
+
+	if (speech->from != camData->target)
+		return;
 
 	camera_update (camEntity);
 	/* this should do an additional check on the platter distance between

commit 0241e6617a46c7d4ca66f3964812803033ba9bac

Fixed rendering bug introduced in commit cd81b21362e140123405b448bbac699b48910bb2
After that, cameras would ignore messages not from their target, but
the positionUpdate message sent on a platter movement was an entity
message () to self instead of an entity_speak (), so the camera would
never get the message that its target had crossed a platter boundary,
and thus it would not update the render cache correctly. (Previously
it had been listening to its own positionUpdate message from crossing
the platter when it updated its position to match its target, but now
that's impossible because a camera that responds to its own position
updates will enter an infinite loop.)

src/component_camera.c
@@ -460,6 +460,11 @@ void component_cameraOrientResponse (EntComponent camera, EntSpeech speech)
 {
 	Entity
 		camEntity = component_entityAttached (camera);
+	cameraComponent
+		camData = component_getData (camera);
+
+	if (speech->from != camData->target)
+		return;
 
 	camera_update (camEntity);
 }
@@ -473,6 +478,7 @@ void component_cameraPositionResponse (EntComponent camera, EntSpeech speech)
 	POSITIONUPDATE
 		update = speech->arg;
 
+	// this has to ignore its own position updates (so there's not a speech->from != camEntity check) because if it doesn't, its own movement will trigger another position update, which it will repond to by updating its position again, etc etc. - xph 2011 12 12
 	if (speech->from != camData->target)
 		return;
 

src/component_position.c
@@ -231,7 +231,8 @@ void position_alignToLevel (Entity e, int spanLevel)
 static void position_messageGroundChange (const EntComponent c, SUBHEX oldGround, SUBHEX newGround)
 {
 	FUNCOPEN ();
-
+	Entity
+		this = component_entityAttached (c);
 	static struct position_update
 		posUpdate;
 	posUpdate.oldGround = oldGround;
@@ -253,7 +254,7 @@ static void position_messageGroundChange (const EntComponent c, SUBHEX oldGround
 		}
 	}
 	posUpdate.difference = mapRelativeDistance (posUpdate.relPosition);
-	entity_message (component_entityAttached (c), NULL, "positionUpdate", &posUpdate);
+	entity_speak (this, "positionUpdate", &posUpdate);
 	memset (&posUpdate, '\0', sizeof (struct position_update));
 	FUNCCLOSE ();
 }