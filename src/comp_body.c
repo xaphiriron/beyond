f2611698ad7c4b06671666e88beb9852a9607b46
xph <xaxster@gmail.com>
2011-12-22 12:13:14 -0600

    Bodies now face in the direction of their orientation vector.
    With the caveat that the position facing code isn't really very
    correct and this results in the facing being off when the camera is
    tilted up or down.

--- src/comp_body.c
+++ src/comp_body.c
@@ -24,19 +24,24 @@ static void drawBody (Entity e)
 {
 	VECTOR3
 		render;
+	const AXES
+		* view;
 	float
 		c, s,
+		facing,
 		radius = 52 / 2;
 	int
 		i = 0;
 	render = position_renderCoords (e);
+	view = position_getViewAxes (e);
+	facing = position_getHeading (e);
 
 	glBindTexture (GL_TEXTURE_2D, 0);
 	glBegin (GL_TRIANGLE_STRIP);
 	while (i < 6)
 	{
-		c = cos ((M_PI * 2 / 6) * i) * radius;
-		s = sin ((M_PI * 2 / 6) * i) * radius;
+		c = cos ((M_PI * 2 / 6) * i + facing) * radius;
+		s = sin ((M_PI * 2 / 6) * i + facing) * radius;
 		if (i % 2)
 			glColor4ub (0xff, 0x99, 0x00, 0xff);
 		else
@@ -46,30 +51,87 @@ static void drawBody (Entity e)
 		i++;
 	}
 	glColor4ub (0x99, 0x55, 0x00, 0xff);
-	glVertex3f (render.x + radius, render.y - 90, render.z);
-	glVertex3f (render.x + radius, render.y, render.z);
+	glVertex3f
+	(
+		render.x + cos (facing) * radius,
+		render.y - 90,
+		render.z + sin (facing) * radius
+	);
+	glVertex3f
+	(
+		render.x + cos (facing) * radius,
+		render.y,
+		render.z + sin (facing) * radius
+	);
 	glEnd ();
 	i = 0;
 	glBegin (GL_TRIANGLE_FAN);
 	glColor4ub (0xff, 0x99, 0x00, 0xff);
 	while (i < 6)
 	{
-		c = cos ((M_PI * 2 / 6) * i) * radius;
-		s = sin ((M_PI * 2 / 6) * i) * radius;
+		c = cos ((M_PI * 2 / 6) * i + facing) * radius;
+		s = sin ((M_PI * 2 / 6) * i + facing) * radius;
 		glVertex3f (render.x + c, render.y - 90, render.z + s);
 		i++;
 	}
-	glVertex3f (render.x + radius, render.y - 90, render.z);
+	glVertex3f
+	(
+		render.x + cos (facing) * radius,
+		render.y - 90,
+		render.z + sin (facing) * radius
+	);
 	glEnd ();
 	i = 6;
 	glBegin (GL_TRIANGLE_FAN);
-	glVertex3f (render.x + radius, render.y, render.z);
+	glVertex3f (render.x + cos (facing) * radius, render.y, render.z + sin (facing) * radius);
 	while (i > 0)
 	{
-		c = cos ((M_PI * 2 / 6) * i) * radius;
-		s = sin ((M_PI * 2 / 6) * i) * radius;
+		c = cos ((M_PI * 2 / 6) * i + facing) * radius;
+		s = sin ((M_PI * 2 / 6) * i + facing) * radius;
 		glVertex3f (render.x + c, render.y, render.z + s);
 		i--;
 	}
+	glEnd ();
+
+	glBegin (GL_LINES);
+	glColor4ub (0x00, 0x99, 0xff, 0xff);
+	glVertex3f
+	(
+		render.x - view->side.x * 52,
+		render.y - view->side.y * 52,
+		render.z - view->side.z * 52
+	);
+	glVertex3f
+	(
+		render.x + view->side.x * 52,
+		render.y + view->side.y * 52,
+		render.z + view->side.z * 52
+	);
+	glColor4ub (0x99, 0x00, 0xff, 0xff);
+	glVertex3f
+	(
+		render.x - view->front.x * 52,
+		render.y - view->front.y * 52,
+		render.z - view->front.z * 52
+	);
+	glVertex3f
+	(
+		render.x + view->front.x * 52,
+		render.y + view->front.y * 52,
+		render.z + view->front.z * 52
+	);
+	glColor4ub (0x99, 0xff, 0x00, 0xff);
+	glVertex3f
+	(
+		render.x - view->up.x * 52,
+		render.y - view->up.y * 52,
+		render.z - view->up.z * 52
+	);
+	glVertex3f
+	(
+		render.x + view->up.x * 52,
+		render.y + view->up.y * 52,
+		render.z + view->up.z * 52
+	);
 	glEnd ();
 };
