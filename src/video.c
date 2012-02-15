75f5555f7649bc625c72e76aaba02b8b0d122c86
xph <xaxster@gmail.com>
2012-02-14 18:21:31 -0600

    who cares about outdated skybox colors

--- src/video.c
+++ src/video.c
@@ -211,11 +211,7 @@ static void video_enableGLmodules ()
 	glExtensions = glGetString (GL_EXTENSIONS);
 	printf ("Using GL version %s with extensions:\n\t%s\n", glVersion, glExtensions);
 
-	//glClearColor (.22, .18, .22, 0.0);
-	//glClearColor (0.20, 0.03, 0.12, 0.0); // dark purple
-	//glClearColor (0.29, 0.10, 0.19, 0.0); // slightly lighter purple
-	//glClearColor (0.84, 0.98, 0.68, 0.0); // bright green
-	glClearColor (0.1215, 0.0588, 0.1843, 0.0); // dark purple (again)
+	glClearColor (0.1215, 0.0588, 0.1843, 0.0); // dark purple
 	glClearDepth (1.0);
 
 	glEnable (GL_DEPTH_TEST);
